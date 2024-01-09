2621d8747fc65e11d2ed4d58e7af710da81a0755
Ramon Estadella Serra <144245823+RamonDataScience@users.noreply.github.com>
2024-01-09 17:36:50 +0100

    commit data-src

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,34 +1,31 @@
+window.onload = function () {
+    const hamburger = document.getElementById("hamburger");
 
+    hamburger.addEventListener("click", function (event) {
+        event.preventDefault();
+        // Deshabilita el botón para evitar múltiples clics
+        hamburger.disabled = true;
 
-const hamburger = document.getElementById("hamburger");
+        // primero poner myTopnav flex direction colun y el height a 300
+        const myTopnav = document.getElementById("myTopnav");
 
-hamburger.addEventListener('click', function(event){
-    event.preventDefault();
-    // Deshabilita el botón para evitar múltiples clics
-    hamburger.disabled = true;
+        // nav__link displayo block
+        const nav__links = Array.from(document.getElementsByClassName("nav__link"));
 
-    // primero poner myTopnav flex direction colun y el height a 300
-    const myTopnav = document.getElementById("myTopnav");
+        // --- EN QUE ESTADO ESTA PARA PONERLO COMO QUIERO
+        if (myTopnav.style.flexDirection === "column") {
+            myTopnav.style.flexDirection = "row";
+            myTopnav.style.height = "80px";
+            nav__links.forEach((nav__link) => {
+                nav__link.style.display = "none";
+            });
+        } else {
+            myTopnav.style.flexDirection = "column";
+            myTopnav.style.height = "300px";
 
-    // nav__link displayo block
-    const nav__links = Array.from(document.getElementsByClassName("nav__link"));
-
-    // --- EN QUE ESTADO ESTA PARA PONERLO COMO QUIERO
-    if(myTopnav.style.flexDirection === 'column'){
-        myTopnav.style.flexDirection = 'row';
-        myTopnav.style.height = '80px';
-        nav__links.forEach(nav__link => {
-            nav__link.style.display = "none";
-        });
-
-    } else {
-        myTopnav.style.flexDirection = 'column';
-        myTopnav.style.height = '300px';
-
-        nav__links.forEach(nav__link => {
-            nav__link.style.display = "block";
-        });
-    }
-
-});
-
+            nav__links.forEach((nav__link) => {
+                nav__link.style.display = "block";
+            });
+        }
+    });
+};
